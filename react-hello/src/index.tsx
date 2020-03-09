1e3a35e2375aa441b844e3847c52509bb94ef80a
Orlando Hernandez <oaraz02@gmail.com>
2020-03-09 15:18:58 -0500

    hello world component

---
+++ react-hello/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function HelloWorld() {
+    return (
+        <div>
+            HelloWorld
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.querySelector('#root')
+);
